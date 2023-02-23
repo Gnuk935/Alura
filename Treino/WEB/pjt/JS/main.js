3d55c033cf3e260448dfb7e104d5d4f8b9bc42f8
Victor Barros <89825674+Gnuk935@users.noreply.github.com>
2023-02-22 23:35:47 -0300

    a
    Proximo: armazenar td json

--- Treino/WEB/pjt/JS/main.js
+++ Treino/WEB/pjt/JS/main.js
@@ -1,11 +1,10 @@
 const botao = document.querySelector('#botao');
 
-
 let lista = [3];
-lista[0] = "Frase 00"
-lista[1] = "Frase 01"
-lista[2] = "Frase 02"
-lista[3] = "Frase 03"
+lista[0] = "O que sinto hoje, veio de você e será eternamente por você!"
+lista[1] = "A gente briga, a gente se ama, a gente vai e a gente volta. A gente é da gente e da gente ninguém tira."
+lista[2] = "O verdadeiro amor nunca se desgasta. Quanto mais se dá mais se tem."
+lista[3] = "Duvida da luz dos astros,"+" De que o sol tenha calor,"+" Duvida até da verdade,"+" Mas confia em meu amor."
 
 botao.onclick = function(){
     let ale = Math.floor(Math.random() * 4)
